01dd9e3f61882d8c6d249bec24e0fede21a95a21
Devashish09 <devashishsoni09@gmail.com>
2016-09-27 11:31:35 +0530

    Add files via upload

---
+++ AngularSQL/Angular.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th , td  {
+  border: 1px solid grey;
+  border-collapse: collapse;
+  padding: 5px;
+}
+
+table tr:nth-child(odd) {
+  background-color: #f1f1f1;
+}
+
+table tr:nth-child(even) {
+  background-color: #ffffff;
+}
+</style>
+<div ng-app="myApp" ng-controller="customersCtrl">
+<table>
+  <tr ng-repeat="x in names">
+    <td>{{ x.Name }}</td>
+    <td>{{ x.Country }}</td>
+  </tr>
+</table>
+</div>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+
+<script>
+var app = angular.module("myApp", []);
+app.controller('customersCtrl',function ($scope,$http) {
+$http.get("host.php")
+    .then(function (response) {$scope.names = response.data.records;});
+});
+
+
+
+/*$http({
+    method : "GET",
+    url : "host.php"
+  }).then(function mySucces(response) {
+      $scope.names = response.data.records;
+    }, function myError(response) {
+      $scope.names = response.statusText;
+  });
+});
+*/
+</script>
+</body></html>
